6099df32d11a97faea9ede0a3763df084d8792f2
이혜진 <floral_berry@naver.com>
2024-04-17 15:56:46 +0900

    Init: Spring 초기 설정

---
+++ backend/piece/src/main/java/com/ssafy/piece/PieceApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.piece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PieceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PieceApplication.class, args);
+	}
+
+}
---
+++ backend/piece/src/test/java/com/ssafy/piece/PieceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.piece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PieceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
