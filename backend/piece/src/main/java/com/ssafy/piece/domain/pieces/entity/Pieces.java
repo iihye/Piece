5a12f18ea08c92ce2ff14dc63768bdbbaa342f71
이혜진 <floral_berry@naver.com>
2024-04-22 15:50:19 +0900

    Feat: pieces entity 작성

--- backend/piece/src/main/java/com/ssafy/piece/domain/pieces/entity/Pieces.java
+++ backend/piece/src/main/java/com/ssafy/piece/domain/pieces/entity/Pieces.java
@@ -1,5 +1,78 @@
 package com.ssafy.piece.domain.pieces.entity;
 
-public class Pieces {
+import com.ssafy.piece.domain.users.entity.Users;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Table(name = "pieces")
+
+public class Pieces {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pieceId;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private Users user;
+
+//    @Column(nullable = false)
+//    private PerformanceType performanceType;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = true)
+    private LocalTime time;
+
+    @Column(nullable = true)
+    private String cast;
+
+    @Column(nullable = true)
+    private String supervision;
+
+    @Column(nullable = true)
+    private String seat;
+
+    @Column(nullable = true)
+    private int price;
+
+    @Column(nullable = true)
+    private String address;
+
+    @Column(nullable = false)
+    private float score;
+
+    @Column(nullable = false)
+    private String comment;
+
+    @Column(nullable = true)
+    private boolean isOpen;
+
+    @Column(nullable = false)
+    private String imageFront;
+
+    @Column(nullable = false)
+    private String imageBack;
 }
