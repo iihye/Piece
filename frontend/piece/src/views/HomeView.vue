c064e023b3c00a59b8e3c77d6052fe1db0f0ab2f
김보라 <bbk4281@naver.com>
2024-04-23 14:32:44 +0900

    Fix: HomeVue 불필요한 코드 삭제

--- frontend/piece/src/views/HomeView.vue
+++ frontend/piece/src/views/HomeView.vue
@@ -1,13 +1,11 @@
 <template>
     <div>
         <RouterView />
-        <SmallButton />
     </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
-import SmallButton from "@/components/button/SmallButton.vue";
 </script>
 
 <style></style>
