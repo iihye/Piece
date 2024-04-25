9088434270864ffa676f3273c46a5185a2fa14cf
김현진 <tcmhdn77@naver.com>
2024-04-25 15:56:34 +0900

    Init : docker setting

--- backend/piece/src/test/java/com/ssafy/piece/PieceApplicationTests.java
+++ backend/piece/src/test/java/com/ssafy/piece/PieceApplicationTests.java
@@ -1,13 +1,12 @@
 package com.ssafy.piece;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PieceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
