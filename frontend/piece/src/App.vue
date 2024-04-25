d8c5c27c3dce857ef2dad2fb8157df0dd508203f
이혜진 <floral_berry@naver.com>
2024-04-25 15:23:49 +0900

    Feat: 공통 router 설정

--- frontend/piece/src/App.vue
+++ frontend/piece/src/App.vue
@@ -1,16 +1,32 @@
 <template>
-  <div>
-    <div class="view">
-		<RouterView />
-	</div>
-  </div>
+    <div class="app">
+        <TheHeader class="TheHeader" />
+        <div>
+            <RouterView />
+        </div>
+        <TheFooter class="TheFooter" />
+    </div>
 </template>
 
 <script setup>
-import { RouterView } from 'vue-router';
-
+import { RouterView } from "vue-router";
+import TheHeader from "./components/common/TheHeader.vue";
+import TheFooter from "./components/common/TheFooter.vue";
 </script>
 
-<style scoped>
-
+<style>
+.app {
+    max-width: 360px;
+    height: 100vh;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.TheHeader {
+}
+.TheFooter {
+    position: fixed;
+    bottom: 0;
+}
 </style>
