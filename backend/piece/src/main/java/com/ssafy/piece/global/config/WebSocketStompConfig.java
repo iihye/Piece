9bf482d4a39e91230d553f3dbfbca357d716dc7a
azin Lee <nuestiloveyou@gmail.com>
2024-04-26 22:05:11 +0900

    Feat: stomp message 전송을 위한 config ì 추가

---
+++ backend/piece/src/main/java/com/ssafy/piece/global/config/WebSocketStompConfig.java
@@ -0,0 +1,36 @@
+package com.ssafy.piece.global.config;
+
+import com.ssafy.piece.domain.chats.interceptor.ChannelInboundInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketStompConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final ChannelInboundInterceptor channelInboundInterceptor;
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/sub"); // 메시지 구독 요청(수신). Subscription URL prefixes
+        config.setApplicationDestinationPrefixes("/pub"); // 메시지 발행 요청(송신)
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+            .setAllowedOriginPatterns("*")
+            .withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(channelInboundInterceptor);
+    }
+}
