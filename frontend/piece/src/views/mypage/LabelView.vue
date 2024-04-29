dd776f3ff7e28cd63c544219c598106dc93d1e4d
이혜진 <floral_berry@naver.com>
2024-04-29 16:41:40 +0900

    Feat: LabelView 리스트 불러오기

--- frontend/piece/src/views/mypage/LabelView.vue
+++ frontend/piece/src/views/mypage/LabelView.vue
@@ -1,31 +1,113 @@
 <template>
-    <h1>칭호</h1>
-
     <div>
-        <h1>여기가 가려지는 뒷배경입니다</h1>
-        <YesNoModal
-            :modalTitle="'칭호를 착용할까요?'"
-            :modalContent="'나의 프로필에서 착용 중인<br>칭호를 보여줄 수 있어요!'"
-            :buttonFail="'취소'"
-            :buttonSuccess="'적용하기'"
-            :handleFailClick="handleFail"
-            :handleSuccessClick="handleSuccess"
-        />
+        <div class="labelview-main-title">나를 표현하는 칭호를 골라주세요</div>
+        <div class="labelview-main-content">칭호를 클릭하여 착용해보세요</div>
+        <div class="labelview-selct-container">
+            <select class="labelview-select-dropbox" v-model="selectedOption">
+                <option value="select-all">전체</option>
+                <option value="select-mine">보유 중인 칭호</option>
+                <option value="select-not">보유하지 않은 칭호</option>
+            </select>
+        </div>
+        <LabelItem
+            v-for="(item, index) in filteredLabelList"
+            :key="index"
+            :isMine="item.myLabels"
+            :isWear="item.wearLabels"
+            :title="item.title"
+            :description="item.description"
+            :handleItemClick="handleItemClick"
+        ></LabelItem>
+
+        <button @click="findMyPageLabelList"></button>
     </div>
 </template>
 
 <script setup>
-import YesNoModal from "@/components/modal/YesNoModal.vue";
+import { ref, computed, onMounted } from "vue";
+import { useUserStore } from "@/stores/user";
+import LabelItem from "@/components/item/LabelItem.vue";
 
-// modal에서 Fail 클릭했을 때 실행되는 함수
-const handleFail = () => {
-    alert("실패 클릭");
+const store = useUserStore();
+// const isMine = ref(true);
+// const isWear = ref(true);
+// const title = ref("영화 애호가");
+// const description = ref("영화 많이 본 사람");
+const mypageLabelList = computed(() => store.getMypageLabelList);
+const selectedOption = ref("select-all");
+
+const filteredLabelList = computed(() => {
+    if (selectedOption.value === "select-all") {
+        return mypageLabelList.value;
+    } else if (selectedOption.value === "select-mine") {
+        return mypageLabelList.value.filter((item) => item.myLabels);
+    } else if (selectedOption.value === "select-not") {
+        return mypageLabelList.value.filter((item) => !item.myLabels);
+    } else {
+        return [];
+    }
+});
+
+const handleItemClick = () => {
+    console.log(mypageLabelList);
+    alert("착용하시겠습니까?");
 };
 
-// modal에서 Success 클릭했을 때 실행되는 함수
-const handleSuccess = () => {
-    alert("성공 클릭");
+const findMyPageLabelList = () => {
+    console.log("버튼 클릭");
+    mypageLabelList.value = store.getMypageLabelList;
 };
+
+// const findMyPageLabelList = function () {
+//     axios({
+//         url: `${import.meta.env.VITE_REST_PIECE_API}/mylabels`,
+//         method: "GET",
+//     })
+//         .then((res) => {
+//             console.log(res.data.data);
+//             mypageLabelList.value = res.data.data;
+//             // console.log(myLabelList.value);
+//         })
+//         .catch((err) => {});
+// };
+
+onMounted(async () => {
+    await store.findMyPageLabelList();
+});
 </script>
 
-<style></style>
+<style>
+.labelview-main-title {
+    font-family: "Semi";
+    font-size: 1.4rem;
+    color: var(--black-color);
+    margin-bottom: 0.6rem;
+}
+
+.labelview-main-content {
+    font-family: "Regular";
+    font-size: 1rem;
+    color: var(--gray2-color);
+    margin: 0;
+    margin-bottom: 1rem;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+}
+
+.labelview-selct-container {
+    display: flex;
+    justify-content: right;
+    margin-bottom: 0.6rem;
+}
+
+.labelview-select-dropbox {
+    width: 10rem;
+    height: 2rem;
+    font-family: "Medium";
+    font-size: 1rem;
+    color: var(--gray2-color);
+    border: 1px solid var(--gray-color);
+    padding-left: 0.2rem;
+}
+</style>
