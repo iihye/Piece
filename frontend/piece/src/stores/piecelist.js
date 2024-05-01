742b7d4ad9a9826f16994e26dd4e8ac346f59dbd
이혜진 <floral_berry@naver.com>
2024-05-01 21:30:43 +0900

    Feat: PieceListMyView list api 연결

--- frontend/piece/src/stores/piecelist.js
+++ frontend/piece/src/stores/piecelist.js
@@ -3,27 +3,36 @@ import { defineStore } from "pinia";
 import router from "@/router";
 import axios from "axios";
 
-export const useUserStore = defineStore(
-    "user",
+export const usePiecelistStore = defineStore(
+    "piecelist",
     () => {
         // =========== STATE ===============
-
+        const piecelistMyList = ref({});
 
         // =========== GETTER ===============
-
-
+        const getPiecelistMyList = computed(() => {
+            return piecelistMyList.value;
+        });
 
         // =========== ACTION ===============
-
-
+        const findPiecelistMyList = function () {
+            axios({
+                url: `${import.meta.env.VITE_REST_PIECE_API}/piecelist/my`,
+                method: "GET",
+            })
+                .then((res) => {
+                    piecelistMyList.value = res.data.data;
+                })
+                .catch((err) => {});
+        };
 
         return {
             // state
-            
+            piecelistMyList,
             // getter
-            
+            getPiecelistMyList,
             // action
-            
+            findPiecelistMyList,
         };
     },
     { persist: true }
--- frontend/piece/src/views/piecelist/PieceListMyView.vue
+++ frontend/piece/src/views/piecelist/PieceListMyView.vue
@@ -21,12 +21,59 @@
     </div>
 
     <!-- list -->
+    <div class="piecelistmyview-list-container">
+        <div class="piecelistmyview-list-grid">
+            <div v-for="(item, index) in filteredMyList" :key="index" class="piecelistmyview-list-item">
+            <ListImageItem
+                :pieceId="item.pieceId"
+                :performanceType="item.performanceType"
+                :frontImg="item.frontImg"
+                :title="item.title"
+                @click="handleItemClick"
+            ></ListImageItem>
+        </div>
+        </div>
+        
+    </div>
+
+    
 </template>
 
 <script setup>
-import { ref, watch, onMounted, onBeforeUnmount } from "vue";
+import { ref, computed, onMounted, onBeforeUnmount } from "vue";
+import { usePiecelistStore } from "@/stores/piecelist";
 import { useRouter } from "vue-router";
 import FilterItem from "@/components/item/FilterItem.vue";
+import ListImageItem from "@/components/item/ListImageItem.vue";
+
+const store = usePiecelistStore();
+
+const piecelistMyList = computed(() => store.getPiecelistMyList);
+const filteredMyList = computed(() => computedFilteredMyList());
+const selectedOption = ref("ALL");
+
+function computedFilteredMyList() {
+    if (selectedOption.value === "ALL") {
+        return piecelistMyList.value;
+    } else if (selectedOption.value === "MOVIE") {
+        return piecelistMyList.value.filter((item) => item.performanceType === "MOVIE");
+    } else if (selectedOption.value === "THEATER") {
+        return piecelistMyList.value.filter((item) => item.performanceType === "THEATER");
+    } else if (selectedOption.value === "MUSICAL") {
+        return piecelistMyList.value.filter((item) => item.performanceType === "MUSICAL");
+    } else if (selectedOption.value === "CONCERT") {
+        return piecelistMyList.value.filter((item) => item.performanceType === "CONCERT");
+    } else if (selectedOption.value === "ETC") {
+        return piecelistMyList.value.filter((item) => item.performanceType === "ETC");
+    } else {
+        return [];
+    }
+}
+
+const handleItemClick = () => {
+    alert("선택");
+};
+
 
 const filterItems = ref([
     {
@@ -68,6 +115,7 @@ const handleItemSelectClick = (index) => {
         }
     }
     filterItems.value[index].isSelect = !filterItems.value[index].isSelect;
+    selectedOption.value = filterItems.value[index].labelType;
 };
 
 // filter
@@ -89,7 +137,8 @@ const handleMouseDown = () => {
     activeDrag.value = true;
 };
 
-onMounted(() => {
+onMounted(async () => {
+    await store.findPiecelistMyList();
     tabMenu.value.addEventListener("mousedown", handleMouseDown);
     document.addEventListener("mouseup", handleMouseUp);
     tabMenu.value.addEventListener("mousemove", handleMouseMove);
@@ -110,6 +159,7 @@ onBeforeUnmount(() => {
     position: relative;
     /* width: 450px; */
     transition: 0.5s ease;
+    margin-bottom: 2rem;
 }
 
 .piecelistmyview-tab-navigation {
@@ -148,5 +198,23 @@ onBeforeUnmount(() => {
 
 .piecelistmyview-tab-menu.dragging .tab-btn {
     pointer-events: none;
+}
+
+/* list */
+.piecelistmyview-list-grid {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(8rem, 1fr));
+    justify-items: center; 
+    grid-gap: 1rem;
+}
+
+.piecelistmyview-list-item {
+    width: auto; 
+}
+
+@media (min-width: 1024px) {
+    .piecelistmyview-list-grid {
+        grid-template-columns: repeat(4, minmax(16rem, 5fr));
+    }
 }
 </style>
