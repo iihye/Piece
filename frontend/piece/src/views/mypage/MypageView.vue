357853b7c1c16dd55a18a6c86c21863373eb19b6
이혜진 <floral_berry@naver.com>
2024-05-02 15:07:40 +0900

    Style: Mypage 임시 css 추가

--- frontend/piece/src/views/mypage/MypageView.vue
+++ frontend/piece/src/views/mypage/MypageView.vue
@@ -1,13 +1,29 @@
 <template>
     <h1>mypage</h1>
-    <RouterLink :to="{ name: 'nickname' }">닉네임 변경</RouterLink>
-    <RouterLink :to="{ name: 'password' }">비밀변호 변경</RouterLink>
-    <RouterLink :to="{ name: 'profileImg' }">프로필이미지 변경</RouterLink>
-    <RouterLink :to="{ name: 'label' }">칭호</RouterLink>
-    <RouterLink :to="{ name: 'piece' }">조각통계</RouterLink>
-    <RouterLink :to="{ name: 'consume' }">소비통계</RouterLink>
+    <RouterLink class="mypageview-link" :to="{ name: 'nickname' }"
+        >닉네임 변경</RouterLink
+    >
+    <RouterLink class="mypageview-link" :to="{ name: 'password' }"
+        >비밀변호 변경</RouterLink
+    >
+    <RouterLink class="mypageview-link" :to="{ name: 'profileImg' }"
+        >프로필이미지 변경</RouterLink
+    >
+    <RouterLink class="mypageview-link" :to="{ name: 'label' }"
+        >칭호</RouterLink
+    >
+    <RouterLink class="mypageview-link" :to="{ name: 'piece' }"
+        >조각통계</RouterLink
+    >
+    <RouterLink class="mypageview-link" :to="{ name: 'consume' }"
+        >소비통계</RouterLink
+    >
 </template>
 
 <script setup></script>
 
-<style></style>
+<style>
+.mypageview-link {
+    display: flex;
+}
+</style>
