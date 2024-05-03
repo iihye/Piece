737725b4d053281af748859e324ec7fa0ab25a21
azin Lee <nuestiloveyou@gmail.com>
2024-05-03 13:34:42 +0900

    Feat: 최근 메세지 n개 불러오는 messageListByCount 메소드 추가

--- backend/piece/src/main/java/com/ssafy/piece/domain/chats/controller/ChatsController.java
+++ backend/piece/src/main/java/com/ssafy/piece/domain/chats/controller/ChatsController.java
@@ -1,13 +1,17 @@
 package com.ssafy.piece.domain.chats.controller;
 
+import com.ssafy.piece.domain.chats.dto.request.ChatsRequestDto;
 import com.ssafy.piece.domain.chats.entity.MongoDBChats;
 import com.ssafy.piece.domain.chats.service.MongoDBChatsService;
+import com.ssafy.piece.global.response.code.SuccessCode;
+import com.ssafy.piece.global.response.structure.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,13 +25,18 @@ public class ChatsController {
     @MessageMapping("/chats/{chatRoomId}") // Send Destination Queue
     public void messageSend(@RequestBody MongoDBChats mongoDBChat) {
         template.convertAndSend("/sub/" + mongoDBChat.getChatRoomId(), mongoDBChat);
-
-        System.out.println("전송 후 저장 프로세스 작동. " + mongoDBChat);
         mongoDBChatsService.saveMongoDBChat(mongoDBChat);
     }
 
     @GetMapping("/chats/list/{chatRoomId}")
     public ResponseEntity messageList(@PathVariable Long chatRoomId) {
-        return ResponseEntity.ok(mongoDBChatsService.listMongoDBChat(chatRoomId));
+        return SuccessResponse.createSuccess(SuccessCode.CHATMESSAGE_LIST_SUCCESS,
+            mongoDBChatsService.listMongoDBChat(chatRoomId));
+    }
+
+    @PostMapping("/chats/list")
+    public ResponseEntity messageListByCount(@RequestBody ChatsRequestDto chatsRequestDto) {
+        return SuccessResponse.createSuccess(SuccessCode.CHATMESSAGE_N_LIST_SUCCESS,
+            mongoDBChatsService.listMessageByCount(chatsRequestDto));
     }
 }
--- backend/piece/src/main/java/com/ssafy/piece/domain/chats/service/MongoDBChatsService.java
+++ backend/piece/src/main/java/com/ssafy/piece/domain/chats/service/MongoDBChatsService.java
@@ -1,9 +1,14 @@
 package com.ssafy.piece.domain.chats.service;
 
+import com.ssafy.piece.domain.chats.dto.request.ChatsRequestDto;
 import com.ssafy.piece.domain.chats.entity.MongoDBChats;
 import com.ssafy.piece.domain.chats.repository.MongoDBChatsRepository;
+
+import java.util.Collections;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,4 +24,11 @@ public class MongoDBChatsService {
     public void saveMongoDBChat(MongoDBChats mongoDBChat) {
         mongoDBChatsRepository.save(mongoDBChat);
     }
+
+    public List<MongoDBChats> listMessageByCount(ChatsRequestDto chatsRequestDto){
+        List<MongoDBChats> listMessageResult= mongoDBChatsRepository.findTopNByChatRoomIdOrderByCreatedAtDesc(chatsRequestDto.getChatRoomId(), PageRequest.of(0, chatsRequestDto.getCount()));
+        Collections.reverse(listMessageResult);
+
+        return listMessageResult;
+    };
 }
