0f0c07427b0ac3000a9ff993999de0675e177f75
azin Lee <nuestiloveyou@gmail.com>
2024-05-07 10:47:43 +0900

    Chore: entity foreign key 관계 제거

--- backend/chat/src/main/java/com/ssafy/chat/entity/MongoDBChatRooms.java
+++ backend/chat/src/main/java/com/ssafy/chat/entity/MongoDBChatRooms.java
@@ -1,5 +1,22 @@
 package com.ssafy.chat.entity;
 
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Document(collection = "chatrooms")
 public class MongoDBChatRooms {
 
+    @Id
+    Long chatRoomId;
 }
--- backend/chat/src/main/java/com/ssafy/chat/entity/MongoDBChats.java
+++ backend/chat/src/main/java/com/ssafy/chat/entity/MongoDBChats.java
@@ -1,5 +1,34 @@
 package com.ssafy.chat.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import java.util.Date;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Document(collection = "chatmessages")
 public class MongoDBChats {
 
+    @Id
+    @Column(nullable = false)
+    Long chatRoomId;
+
+    @Column(nullable = false)
+    Long senderId;
+
+    @Column(nullable = false)
+    String content;
+
+    @Column(nullable = false)
+    Date createdAt;
 }
