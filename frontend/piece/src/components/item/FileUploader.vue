12c7ac3c95b46b2cbf09f51c0aa57acb598d6283
이혜진 <floral_berry@naver.com>
2024-05-08 14:53:57 +0900

    Style: ProfileImgView CSS 완성

--- frontend/piece/src/components/item/FileUploader.vue
+++ frontend/piece/src/components/item/FileUploader.vue
@@ -1,22 +1,29 @@
 <template>
     <div>
-        <input type="file" @change="uploadImage" ref="fileInput" style="display: none">
-        <button @click="triggerFileUpload">{{ buttonText }}</button>
+        <input
+            type="file"
+            @change="uploadImage"
+            ref="fileInput"
+            style="display: none"
+        />
+        <button class="fileuploader-button" @click="triggerFileUpload">
+            {{ buttonText }}
+        </button>
     </div>
 </template>
 
 <script setup>
-import { ref, defineProps, defineEmits } from 'vue';
-import axios from 'axios';
-import { getPreSignedUrl } from '@/stores/fileupload';
+import { ref, defineProps, defineEmits } from "vue";
+import axios from "axios";
+import { getPreSignedUrl } from "@/stores/fileupload";
 
 const props = defineProps({
     buttonText: {
         type: String,
-        default: 'Upload Image'
-    }
+        default: "Upload Image",
+    },
 });
-const emits = defineEmits(['uploaded', 'error']);
+const emits = defineEmits(["uploaded", "error"]);
 
 const fileInput = ref(null);
 
@@ -27,7 +34,7 @@ function triggerFileUpload() {
 async function uploadImage(event) {
     const file = event.target.files[0];
     if (!file) {
-        emits('error', '선택된 파일 없슴');
+        emits("error", "선택된 파일 없슴");
         return;
     }
 
@@ -38,26 +45,42 @@ async function uploadImage(event) {
         console.log("Presigned URL : ", url);
 
         if (!url) {
-            emits('error', 'URL 받아오기 실패');
+            emits("error", "URL 받아오기 실패");
             return;
         }
 
         const uploadResponse = await axios.put(url, file, {
             headers: {
-                'Content-Type': file.type
-            }
+                "Content-Type": file.type,
+            },
         });
 
         console.log("Upload response:", uploadResponse); // 업로드 응답 로그
 
         if (uploadResponse.status === 200 || uploadResponse.status === 204) {
-            emits('uploaded', URL.createObjectURL(file));
+            emits("uploaded", URL.createObjectURL(file));
         } else {
-            emits('error', 'Upload failed with status: ' + uploadResponse.status);
+            emits(
+                "error",
+                "Upload failed with status: " + uploadResponse.status
+            );
         }
     } catch (error) {
-        console.error('파일 업로드 하면서 에러 생겼음', error);
-        emits('error', error.message);
+        console.error("파일 업로드 하면서 에러 생겼음", error);
+        emits("error", error.message);
     }
 }
 </script>
+
+<style>
+.fileuploader-button {
+    background-color: var(--main-color);
+    border: 0;
+    border-radius: 0.625rem;
+    height: 2.5rem;
+    width: 7.5rem;
+    color: var(--white-color);
+    font-family: "Semi";
+    font-size: 1rem;
+}
+</style>
