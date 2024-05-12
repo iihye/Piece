a93f272140e97464edaaee733f8377660bab24e4
azin Lee <nuestiloveyou@gmail.com>
2024-05-12 19:44:22 +0900

    Chore: FeignClient를 사용하기 위한 annotation 추가

--- backend/chat/src/main/java/com/ssafy/chat/ChatApplication.java
+++ backend/chat/src/main/java/com/ssafy/chat/ChatApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(basePackages = {"com.ssafy.chat.global.client"})
 public class ChatApplication {
 
 	public static void main(String[] args) {
