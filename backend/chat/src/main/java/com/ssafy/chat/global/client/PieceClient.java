343e223ceebcf053980d2a31158798b3e9fea933
azin Lee <nuestiloveyou@gmail.com>
2024-05-12 19:46:54 +0900

    Feat: Fei타 프로젝트로부터 entity를 받아오기 위한 FeignClient 메소드 추가

--- backend/chat/src/main/java/com/ssafy/chat/global/client/PieceClient.java
+++ backend/chat/src/main/java/com/ssafy/chat/global/client/PieceClient.java
@@ -1,11 +1,18 @@
 package com.ssafy.chat.global.client;
 
+import com.ssafy.chat.dto.response.CultureResponseDto;
 import com.ssafy.chat.global.config.FeignClientConfig;
+import jakarta.ws.rs.Path;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "piece", url = "${external.piece.host}", configuration = FeignClientConfig.class)
 public interface PieceClient {
+    @GetMapping("/cultures/{cultureId}")
+    CultureResponseDto getCulture(@PathVariable Long cultureId);
 
-
+    @GetMapping("/labels/{labelId}")
+    ResponseEntity<Object> getLabel(@PathVariable Long labelId);
 }
