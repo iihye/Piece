4f60175ad79a9cdb9392a7693cd57f762d51ab75
김보라 <bbk4281@naver.com>
2024-05-13 09:09:37 +0900

    Refactor: 어노테이션 삭제

--- backend/piece/src/main/java/com/ssafy/piece/domain/pieces/controller/PieceSerchController.java
+++ backend/piece/src/main/java/com/ssafy/piece/domain/pieces/controller/PieceSerchController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/search")
