dbacb0967c1c15543de7c91d8c6420505e9114cb
azin Lee <nuestiloveyou@gmail.com>
2024-05-14 09:41:02 +0900

    Rename: ChatRoom 생성 요청을 1:1, ì Open 여부에 따라 분리하면서 Request format도 변경

---
+++ backend/chat/src/main/java/com/ssafy/chat/dto/request/OpenChatRoomsCreateRequestDto.java
@@ -0,0 +1,18 @@
+package com.ssafy.chat.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OpenChatRoomsCreateRequestDto {
+    Long cultureId;
+    String chatRoomName;
+}
