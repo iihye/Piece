a71fa9bedcd176b1227e2f6225ab42c7856c9706
azin Lee <nuestiloveyou@gmail.com>
2024-05-14 09:49:11 +0900

    Feat: FeignClient 사용에 따라 client 로직 추가

--- backend/chat/src/main/java/com/ssafy/chat/controller/ChatsController.java
+++ backend/chat/src/main/java/com/ssafy/chat/controller/ChatsController.java
@@ -1,9 +1,14 @@
 package com.ssafy.chat.controller;
 
-import com.ssafy.chat.dto.MongoDBChatMessage;
+import com.ssafy.chat.dto.request.MessageRequestDto;
 import com.ssafy.chat.dto.request.ChatsRequestDto;
+import com.ssafy.chat.dto.response.LabelResponseDto;
+import com.ssafy.chat.dto.response.MessageResponseDto;
+import com.ssafy.chat.dto.response.UserFeignResponseDto;
+import com.ssafy.chat.dto.response.UserResponseDto;
 import com.ssafy.chat.entity.MongoDBChats;
-import com.ssafy.chat.global.annotation.AuthenticatedUser;
+import com.ssafy.chat.global.client.PieceClient;
+import com.ssafy.chat.global.client.UserClient;
 import com.ssafy.chat.global.response.code.SuccessCode;
 import com.ssafy.chat.global.response.structure.SuccessResponse;
 import com.ssafy.chat.service.MongoDBChatsService;
@@ -24,19 +29,42 @@ public class ChatsController {
     private final SimpMessageSendingOperations template;
     private final MongoDBChatsService mongoDBChatsService;
 
+    private final PieceClient pieceClient;
+    private final UserClient userClient;
+
     @MessageMapping("/chats/{chatRoomId}") // Send Destination Queue
-    public void messageSend(@RequestBody MongoDBChatMessage mongoDBChatMessage
+    public void messageSend(@RequestBody MessageRequestDto messageRequestDto
         ) {
-        System.out.println("🍿"+mongoDBChatMessage);
         // senderId는 vue.js에서 처리
+
+        UserResponseDto userInfo=userClient.getUser(messageRequestDto.getSenderId()).getData();
+
+        String userTitle=pieceClient.getLabel(userInfo.getLabelId()).getData().getTitle();
+
+        // mongoDB 저장용
         MongoDBChats mongoDBChat = MongoDBChats.builder()
-            .chatRoomId(mongoDBChatMessage.getChatRoomId())
-            .senderId(mongoDBChatMessage.getSenderId())
-            .content(mongoDBChatMessage.getContent())
-            .createdAt(mongoDBChatMessage.getCreatedAt())
+            .chatRoomId(messageRequestDto.getChatRoomId())
+            .senderId(messageRequestDto.getSenderId())
+            .content(messageRequestDto.getContent())
+            .createdAt(messageRequestDto.getCreatedAt())
+            .profileImage(userInfo.getProfileImage())
+            .nickname(userInfo.getNickname())
+            .title(userTitle)
             .build();
 
-        template.convertAndSend("/sub/" + mongoDBChat.getChatRoomId(), mongoDBChat);
+        // response 전용
+        MessageResponseDto messageResponseDto=MessageResponseDto.builder()
+                .chatRoomId(messageRequestDto.getChatRoomId())
+                .senderId(messageRequestDto.getSenderId())
+                .content(messageRequestDto.getContent())
+                .createdAt(messageRequestDto.getCreatedAt())
+                .profileImage(userInfo.getProfileImage())
+                .nickname(userInfo.getNickname())
+                .title(userTitle)
+                .build();
+
+        template.convertAndSend("/sub/" + messageRequestDto.getChatRoomId(), messageResponseDto);
+
         mongoDBChatsService.saveMongoDBChat(mongoDBChat);
     }
 
