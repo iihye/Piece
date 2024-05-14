27ea19d8f27af44f528d235e9e343b6959b894d2
김현진 <tcmhdn77@naver.com>
2024-05-15 01:11:34 +0900

    Refactor: fileupload

--- backend/user/src/main/java/com/ssafy/user/controller/UserFileUploadController.java
+++ backend/user/src/main/java/com/ssafy/user/controller/UserFileUploadController.java
@@ -4,12 +4,13 @@ import com.ssafy.user.global.annotation.AuthenticatedUser;
 import com.ssafy.user.global.response.code.SuccessCode;
 import com.ssafy.user.global.response.structure.SuccessResponse;
 import com.ssafy.user.service.UserFileUploadService;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,11 +22,11 @@ public class UserFileUploadController {
 
     private final UserFileUploadService userFileUploadService;
 
-
-    @PutMapping("/save/{s3filepath}")
-    public ResponseEntity<Object> saveS3Path(@AuthenticatedUser Long userId, @PathVariable(name = "s3filepath") String s3filepath) {
-        log.info("s3 filepath : {}", s3filepath);
-        userFileUploadService.saveUserProfileImage(userId.toString(), s3filepath);
+    @PutMapping("/save/profileimage")
+    public ResponseEntity<Object> saveS3Path(@AuthenticatedUser Long userId, @RequestBody Map<String, String> body) {
+        String s3path = body.get("s3path");
+        log.info("s3path: {}", s3path);
+        userFileUploadService.saveUserProfileImage(userId.toString(), s3path);
         return SuccessResponse.createSuccess(SuccessCode.SAVE_IMAGE_SUCCESS);
     }
 
