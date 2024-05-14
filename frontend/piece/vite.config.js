b4035608e9f4e76c5f38192c56cafbdaf0cdc74a
이혜진 <floral_berry@naver.com>
2024-05-15 02:15:59 +0900

    Feat: PWA public 파일 수정

--- frontend/piece/vite.config.js
+++ frontend/piece/vite.config.js
@@ -1,20 +1,48 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      eventsource:
-      "./node_modules/sockjs-client/lib/transport/browser/eventsource.js",
-      events: "./node_modules/sockjs-client/lib/event/emitter.js",
-      crypto: "./node_modules/sockjs-client/lib/utils/browser-crypto.js",
-    }
-  }
-})
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "piece",
+                short_name: "piece",
+                description: "piece",
+                theme_color: "#ff9494",
+                icons: [
+                    {
+                        src: "icons/icon-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "icons/icon-256x256.png",
+                        sizes: "256x256",
+                        type: "image/png",
+                    },
+                    {
+                        src: "icons/icon-384x384.png",
+                        sizes: "384x384",
+                        type: "image/png",
+                    },
+                    {
+                        src: "icons/icon-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+});
