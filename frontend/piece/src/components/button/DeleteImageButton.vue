fa48e08890694d33ad73ef071e5961b82f8c3c3c
김현진 <tcmhdn77@naver.com>
2024-05-15 04:07:09 +0900

    Refactor: fileupload

--- frontend/piece/src/components/button/DeleteImageButton.vue
+++ frontend/piece/src/components/button/DeleteImageButton.vue
@@ -1,7 +1,3 @@
-<!-- <template>
-    <button @click="deleteProfileImage" class="delete-image-btn">프로필 이미지 삭제</button>
-</template> -->
-
 <template>
     <div class="fileuploader-button">
         <button @click="uploadImage">{{ roundButtonContent }} 사진 삭제하기</button>
