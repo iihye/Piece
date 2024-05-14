f4d9ceada06fe309197ebc05310dd99310103fe5
이혜진 <floral_berry@naver.com>
2024-05-14 10:43:11 +0900

    Style: LabelView height 수정

--- frontend/piece/src/views/mypage/LabelView.vue
+++ frontend/piece/src/views/mypage/LabelView.vue
@@ -138,7 +138,7 @@ onMounted(async () => {
 .labelview-main-container {
     display: flex;
     flex-direction: column;
-    height: calc(100vh - 7.25rem);
+    height: calc(100vh - 8.25rem);
     padding-top: 1rem;
     margin: 0 1rem 0 1rem;
 }
