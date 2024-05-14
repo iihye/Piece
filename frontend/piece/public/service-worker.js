d74fca0ab71f0c131d165ef5b0d5981c9831497a
이혜진 <floral_berry@naver.com>
2024-05-15 02:54:24 +0900

    Refactor: service worker 관련 주석 처리

--- frontend/piece/public/service-worker.js
+++ frontend/piece/public/service-worker.js
@@ -1,5 +1,5 @@
 self.addEventListener("fetch", (e) => {
-    console.log(e.request.url);
+    // console.log(e.request.url);
     // e.respondWith(
     //   caches.match(e.request).then((response) => response || fetch(e.request)),
     // );
