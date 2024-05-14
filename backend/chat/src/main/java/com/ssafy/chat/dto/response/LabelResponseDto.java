51c2c79abf26437d09932d03fe232ebef41866a3
azin Lee <nuestiloveyou@gmail.com>
2024-05-14 10:10:33 +0900

    Feat: 칭호를 받아올 ãdto 추가

---
+++ backend/chat/src/main/java/com/ssafy/chat/dto/response/LabelResponseDto.java
@@ -0,0 +1,17 @@
+package com.ssafy.chat.dto.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LabelResponseDto {
+    String title;
+}
