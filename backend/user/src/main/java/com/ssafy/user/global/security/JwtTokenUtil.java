0b4e9aadb8658f235a9867cc8da9152d3b032b12
김현진 <tcmhdn77@naver.com>
2024-05-15 20:19:46 +0900

    Fix: jwt version 수정

--- backend/user/src/main/java/com/ssafy/user/global/security/JwtTokenUtil.java
+++ backend/user/src/main/java/com/ssafy/user/global/security/JwtTokenUtil.java
@@ -3,6 +3,8 @@ package com.ssafy.user.global.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import org.springframework.stereotype.Component;
 
@@ -13,9 +15,30 @@ public class JwtTokenUtil {
 //    private String secretKey;
     public String secretKey = "ssafy1234"; // 환경 변수로 관리하는 것이 좋습니다.
 
+//    public Long getUserIdFromToken(String token) {
+//        Claims claims = Jwts.parser()
+//            .setSigningKey(secretKey.getBytes())
+//            .parseClaimsJws(token)
+//            .getBody();
+//        return Long.parseLong(claims.getSubject());
+//    }
+//
+//    public String generateToken(String username) {
+//        return Jwts.builder()
+////            .setClaims()
+//            .setSubject(username)
+//            .setIssuedAt(new Date())
+//            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10시간 후 만료
+//            .signWith(SignatureAlgorithm.HS256, secretKey.getBytes())
+//            .compact();
+//    }
+
+
+
     public Long getUserIdFromToken(String token) {
-        Claims claims = Jwts.parser()
-            .setSigningKey(secretKey.getBytes())
+        Claims claims = Jwts.parserBuilder()
+            .setSigningKey(secretKey)
+            .build()
             .parseClaimsJws(token)
             .getBody();
         return Long.parseLong(claims.getSubject());
@@ -23,10 +46,9 @@ public class JwtTokenUtil {
 
     public String generateToken(String username) {
         return Jwts.builder()
-//            .setClaims()
             .setSubject(username)
             .setIssuedAt(new Date())
-            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10시간 후 만료
+            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
             .signWith(SignatureAlgorithm.HS256, secretKey.getBytes())
             .compact();
     }
