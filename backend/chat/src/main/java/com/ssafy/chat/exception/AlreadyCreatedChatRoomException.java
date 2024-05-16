c974c5df379a64219eb0ab389f6af8a4da8968cf
이아진 <nuestiloveyou@gmail.com>
2024-05-16 09:25:16 +0900

    Style: 컨벤션에 맞게 리팩토링

--- backend/chat/src/main/java/com/ssafy/chat/exception/AlreadyCreatedChatRoomException.java
+++ backend/chat/src/main/java/com/ssafy/chat/exception/AlreadyCreatedChatRoomException.java
@@ -4,7 +4,6 @@ import com.ssafy.chat.global.response.code.ErrorCode;
 import com.ssafy.chat.global.response.exception.BusinessException;
 
 public class AlreadyCreatedChatRoomException extends BusinessException {
-
     public AlreadyCreatedChatRoomException() {
         super(ErrorCode.INVALID_REQUEST);
     }
