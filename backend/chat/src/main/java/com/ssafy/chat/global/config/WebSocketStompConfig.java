7fca3cc4b4ccd0390a6d8d01727028ab6029968d
김근형 <ppoyam_2@naver.com>
2024-05-17 12:29:47 +0900

    Fix: 변겅 사항 커밋하였습니다.

---
+++ backend/chat/src/main/java/com/ssafy/chat/global/config/WebSocketStompConfig.java
@@ -0,0 +1,27 @@
+package com.ssafy.chat.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketStompConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/sub"); // 메시지 구독 요청(수신). Subscription URL prefixes
+        config.setApplicationDestinationPrefixes("/pub"); // 메시지 발행 요청(송신)
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+            .withSockJS();
+    }
+}
---
+++ backend/piece/src/main/java/com/ssafy/piece/global/security/JwtTokenUtil.java
@@ -0,0 +1,57 @@
+//package com.ssafy.piece.global.security;
+//
+//import io.jsonwebtoken.Claims;
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureAlgorithm;
+//
+//
+//import java.util.Date;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class JwtTokenUtil {
+//    public String secretKey = "ssafy1234"; // 환경 변수로 관리하는 것이 좋습니다.
+//
+//    public Long getUserIdFromToken(String token) {
+//        Claims claims = Jwts.parser()
+//            .setSigningKey(secretKey.getBytes())
+//            .parseClaimsJws(token)
+//            .getBody();
+//        return Long.parseLong(claims.getSubject());
+//    }
+//
+//    public String generateToken(String username) {
+//        return Jwts.builder()
+////            .setClaims()
+//            .setSubject(username)
+//            .setIssuedAt(new Date())
+//            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10시간 후 만료
+//            .signWith(SignatureAlgorithm.HS256, secretKey)
+//            .compact();
+//    }
+//
+//    public boolean validateToken(String token, String username) {
+//        final String tokenUsername = getUsernameFromToken(token);
+//        return (username.equals(tokenUsername) && !isTokenExpired(token));
+//    }
+//
+//    private Date getExpirationDateFromToken(String token) {
+//        return getAllClaimsFromToken(token).getExpiration();
+//    }
+//
+//    private Claims getAllClaimsFromToken(String token) {
+//        return Jwts.parser()
+//            .setSigningKey(secretKey)
+//            .parseClaimsJws(token)
+//            .getBody();
+//    }
+//
+//    private boolean isTokenExpired(String token) {
+//        final Date expiration = getExpirationDateFromToken(token);
+//        return expiration.before(new Date());
+//    }
+//
+//    public String getUsernameFromToken(String token) {
+//        return getAllClaimsFromToken(token).getSubject();
+//    }
+//}
